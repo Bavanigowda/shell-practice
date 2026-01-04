58ce63d135428bf19d648f1ebad68ca865928405
Bavani Gowda <bhavani810527@gmail.com>
2026-01-04 23:30:58 +0530

    Ramesh and Suresh conversation

---
+++ 02-hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo"Ramesh:: Hi Suresh, How are you?"
+echo"suresh:: Hello Ramesh, I am good, how are you?
+echo"Ramesh:: What are you Learning today"
+echo"Suresh:: Devops plus AWS"
