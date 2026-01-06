3c9a76809d4c4fd8d0fc3f117659d551478ea58d
Bavani Gowda <bhavani810527@gmail.com>
2026-01-06 20:17:08 +0530

    some text

---
+++ 04-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON=$1
+PERSON=$2
+
+echo "$PERSON1:: welcome to india $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, Finally not an ZOOM call"
+echo "$PERSON1:: So, do you have an Deal"
+echo "$PERSON2:: yes, if you make it "
+
