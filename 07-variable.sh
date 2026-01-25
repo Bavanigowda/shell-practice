b815670b05a1e32c37b34ff40563a1768560310f
Bavani Gowda <bhavani810527@gmail.com>
2026-01-25 12:41:54 +0530

    variables

--- 07-variable.sh
+++ 07-variable.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#Date =$date
+#Date=$date
 
 START_TIME=$(date +%s)
 
 Sleep 10 &
 
 End_TIME=$(date +%s)
-Total_Time=$($END_TIME-$START-TIME)
+TOTAL_TIME=$($END_TIME-$START-TIME)
 
 echo "Script executed in: $Total_TIME seconds"
 
