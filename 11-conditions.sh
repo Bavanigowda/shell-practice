83c0965c54d77fbef58da6fc248902f75f28bd15
Bavani Gowda <bhavani810527@gmail.com>
2026-01-26 19:10:30 +0530

    variable

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -4,7 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
    echo "given number $NUMBER is less than 10"
-elif [ $Number -eq 10]; then 
+elif [ $NUMBER -eq 10]; then 
+   echo "given number $NUMBER is equal to 10"
 else
    echo "given number $NUMBER is greater than to 10"
 fi
