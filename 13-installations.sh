a98c825a8a8b48983b8f8da4c561c9f6d0ee417a
Bavani Gowda <bhavani810527@gmail.com>
2026-01-26 22:16:53 +0530

    variable

--- 13-installations.sh
+++ 13-installations.sh
@@ -4,13 +4,15 @@ USERID=$(id  -u)
 
 if [ $USERID -ne 0 ]; then
    echo "Error:: please run this script with root priviledge"
+   exit 1 # failure is other than 
    fi
 
    dnf install mysql -y
 
    if [ $? -ne 0 ]; then
       echo "Error:: Installing MySQl is failure"
+      exit 1 # failure is other than 
     else 
       echo "installing mysql is success"
+      exit 1 # failure is other than 
     fi
-    
